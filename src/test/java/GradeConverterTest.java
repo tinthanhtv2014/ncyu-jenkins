dea13211ab06f2e37504dbe4af77831618109d1a
Hoc vien <tinthanhtv2014@gmail.com>
2024-05-20 13:48:59 +0700

    git commit -m update

--- src/test/java/GradeConverterTest.java
+++ src/test/java/GradeConverterTest.java
@@ -24,4 +24,10 @@ public class GradeConverterTest {
     String actual = converter.convert(90);
     assertEquals(expected, actual);
   }
+
+  public void testConvert3() {
+    String expected = "A";
+    String actual = converter.convert(-10);
+    assertEquals(expected, actual);
+  }
 }
